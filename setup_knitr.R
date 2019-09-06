75eb8c4b3551452afea4e20992ce7c8ede155a5b
Fernando Mayer <fernandomayer@gmail.com>
2019-09-06 18:50:08 -0300

    Slides RNG uniforme final

--- setup_knitr.R
+++ setup_knitr.R
@@ -5,9 +5,10 @@ opts_chunk$set(
     tidy = FALSE,
     comment = "#",
     collapse = TRUE, ## colapsa chunks em R Markdown
-    ## fig.width = 10,
-    ## fig.height = 8,
+    fig.width = 10,
+    fig.height = 8,
     fig.align = "center",
+    out.width = "80%",
     ## dpi = 80, ## aumentar o dpi para exibir maior.
     ## dev = "png",
     cache.path = "cache/",
